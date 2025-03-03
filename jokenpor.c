9bacc5d8cb6c5a26fe481a61e8083d9f5ea1f3aa
Daniel Diniz de Oliveira Alves <danieldiniz0304@gmail.com>
2025-03-03 01:30:24 +0000

    jokenpor

---
+++ jokenpor.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int leitura()
+{
+    int valor;
+    printf("JOGO DE JOKENPO\n\nEscolha uma jogada:\n1.Pedra\n2.Papel\n3.Tesoura\n\n");
+    scanf("%d", &valor);
+
+    return valor;
+}
+
+void jokenpo(int escolha, char ID[8])
+{
+    switch (escolha)
+    {
+    case 1:
+
+        printf("%s: Pedra\n", ID);
+        break;
+
+    case 2:
+
+        printf("%s: Papel\n", ID);
+        break;
+
+    case 3:
+
+        printf("%s: Tesoura\n", ID);
+        break;
+
+    default:
+
+        printf("%s: Invalido\n", ID);
+        break;
+    }
+
+}
+
+int main()
+{
+    char IDJ[8] = "jogador";
+    char IDM[8] = "maquina";
+    int jogador;
+    int maquina;
+
+    srand(time(0));
+    maquina = rand() % 3+1;
+    jogador = leitura();
+    jokenpo(jogador,IDJ);
+    jokenpo(maquina,IDM);
+
+}
