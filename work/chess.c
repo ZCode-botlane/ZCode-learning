311317015106f25fad7543265a85071f43c7240c
Daniel Diniz de Oliveira Alves <danieldiniz0304@gmail.com>
2025-03-06 21:19:43 +0000

    chess finish

--- work/chess.c
+++ work/chess.c
@@ -1,48 +1,66 @@
 #include <stdio.h>
 
-int main()
+void andarPecas(int n, char direcao[10], int peca, char dirCavalo[6])
 {
-    int peca;
-    int casas;
-
-    printf("digite uma peça(1.torre,2.bispo,3.rainha,4.cavalo): ");
-    scanf("%d", &peca);
-    printf("digite o numero de casas: ");
-    scanf("%d", &casas);
-
-    if(peca == 1)
+    if(peca != 4)
     {
-        for(int i=0;i<=casas;i++)
+        if(n > 0)
         {
-            printf("cima\n");
-        }
-    }else if(peca == 2)
-    {
-        for(int i=0;i<=casas;i++)
-        {
-            printf("cima,direita\n");
-        }
-    }else if(peca == 3)
-    {
-        for(int i=0;i<=casas;i++)
-        {
-            printf("esquerda\n");
-        }
-    }else if(peca == 4)
-    {
-        for(int i = 1; i<=casas; i++)
-        {
-            int j = casas;
-            printf("baixo \n");
-            while( j == i)
+            printf("%s", direcao);
+            if(peca == 2)
             {
                 printf("esquerda\n");
+            }
+            andarPecas(n-1, direcao, peca, dirCavalo);
+        }
+    }else{
+        for(int i = 1; i<=n; i++)
+        {
+            int j = n;
+            printf("%s \n", direcao);
+            while( j == i)
+            {
+                printf("%s\n",  dirCavalo);
                 j++;
             }
     
         }
     }
+}
+
+
+int main(){
+
+
+    int peca;
+    int casas;
+    char dirCavalo[10];
+
+    printf("digite uma peça(1.torre,2.bispo,3.rainha,4.cavalo): ");
+    scanf("%d", &peca);       
+    printf("digite o numero de casas: ");
+    scanf("%d", &casas);
+
+    switch (peca)
+    {
+    case (4):
+        printf("digite a segunda direção do cavalo (esquerda, direita): ");
+        scanf("%s", dirCavalo);
+        andarPecas(casas, "cima", peca, dirCavalo);
+        break;
+    case(1):
+        andarPecas(casas, "cima", peca, dirCavalo);
+        break;
+    case(2):
+        andarPecas(casas, "cima, ", peca, dirCavalo);
+        break;
+    case(3):
+        andarPecas(casas, "esquerda", peca, dirCavalo);
+        break;
+    default:
+        printf("Invalido");
+        break;
      
     return 0;
-
+   }
 }
